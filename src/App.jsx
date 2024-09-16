d2b674a44381756cb0bc6b2e23a85c197d5d076c
skkhan5 <yousafzai5588@gmail.com>
2024-09-16 05:18:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header.jsx";
+import Products from "./pages/Products.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     
+      <Header   />
+     
+      <Routes>
+        <Route path="/" element={<Products/>} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+// import { BrowserRouter, Route, Routes } from "react-dom/client";
+// import Home from "./pages/Home";
+// import Header from "./components/Header.jsx";
+// import Products from "./pages/Products.jsx";
+// import ProductDetail from "./pages/ProductDetail.jsx";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/products/:id" element={<ProductDetail />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
